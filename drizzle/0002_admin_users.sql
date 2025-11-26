a908b769698d9e5fc452aefc8bf2c2126661c495
hamzardl <157745343+hamzardl@users.noreply.github.com>
2025-11-26 09:49:52 -0500

    Delete drizzle directory
    Delete drizzle directory

--- drizzle/0002_admin_users.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `ships` MODIFY COLUMN `status` enum('docked','sailing','lookingForAFight') NOT NULL;--> statement-breakpoint
-ALTER TABLE `users` ADD `is_admin` boolean DEFAULT false NOT NULL;
